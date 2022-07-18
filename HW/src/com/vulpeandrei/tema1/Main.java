c21cbfbd637327671207b5343800a526ec2d1afa
Andrei Vulpe <andreivulpe02@yahoo.com>
2022-07-18 18:47:06 +0300

    Update Main.java

--- HW/src/com/vulpeandrei/tema1/Main.java
+++ HW/src/com/vulpeandrei/tema1/Main.java
@@ -5,14 +5,14 @@ public class Main {
         Account account = new Account();
         account.deposit(100);
 
-        // Uncomment line 7 for checking how NotEnoughMoneyException works
+        // Uncomment line 9 for checking how NotEnoughMoneyException works
 //         account.withdraw(200);
         account.deposit(200);
         account.withdraw(200);
 
         account.linkToNationalId(1990101409143L);
 
-        // Uncomment line 14 for checking how InvalidNationaIdException works
+        // Uncomment line 16 for checking how InvalidNationaIdException works
 //        account.linkToNationalId(2990101409143L);
     }
 }
